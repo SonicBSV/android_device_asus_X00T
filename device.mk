ebffa20d7c28893d742112eb242f5570b1b01273
SonicBSV <sonicbsv@gmail.com>
2019-04-13 01:11:06 +0300

    Revert "add SnapdragonCamera"
    This reverts commit 8a25d681e1b3c2a576df2ac7b63c49d7c3602c3e.

--- device.mk
+++ device.mk
@@ -196,9 +196,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    SnapdragonCamera
-    
-#    Snap
+    Snap
 #    vendor.qti.hardware.camera.device@1.0 \
 #    libmmlib2d_interface \
 #    libhal_dbg \
