88e13ff6dffb472bb3555b4a2a29d9993b7c8471
SonicBSV <sonicbsv@gmail.com>
2019-05-08 18:38:12 +0300

    rootdir: add fix baseband (thx @Anomalchik),del duplicate fingerprint script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,10 +52,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.fingerprint.sh
+LOCAL_MODULE       := fix_baseband.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.fingerprint.sh
+LOCAL_SRC_FILES    := bin/fix_baseband.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
