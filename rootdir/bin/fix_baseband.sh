af8ff9602ce8382a020a212defa9053c2474ba24
SonicBSV <sonicbsv@gmail.com>
2019-05-26 23:36:26 +0300

    del fix_baseband

--- rootdir/bin/fix_baseband.sh
+++
@@ -1,9 +0,0 @@
-#! /vendor/bin/sh
-
-VER_INFO=/vendor/firmware_mnt/verinfo/ver_info.txt
-
-if [ -f $VER_INFO ]; then
-    BB_VER=$(grep '"modem":' $VER_INFO | cut -d '"' -f4)
-    setprop gsm.version.baseband $BB_VER
-fi
-
