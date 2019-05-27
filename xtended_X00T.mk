2e348187ab57e2f483f43ac1e17d9e17f6ce245a
SonicBSV <sonicbsv@gmail.com>
2019-05-27 05:08:11 +0300

    xtended_X00T rebase

--- xtended_X00T.mk
+++ xtended_X00T.mk
@@ -21,6 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common xtended stuff.
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from X00T device
@@ -30,26 +34,18 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
 PRODUCT_NAME := xtended_X00T
+PRODUCT_MODEL := ZenFone Max Pro M1
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.xtended.maintainer=SonicBSV
+    
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1181 release-keys" \
-    DEVICE_MAINTAINERS="SonicBSV"
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190305.002 5240760 release-keys" 
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1905.053-20190513:user/release-keys
-
-# The following system and vendor props will be set by vendor init
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
-    ro.product.device \
-    ro.product.model \
-    ro.product.name
-
-PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
-    ro.vendor.product.device \
-    ro.vendor.product.model \
-    ro.vendor.product.name
+BUILD_FINGERPRINT := google/blueline/blueline:9/PQ2A.190305.002/5240760:user/release-keys
