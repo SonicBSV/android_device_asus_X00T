59a22f70b0fbea4886b55f362231e29ad6f29d7a
SonicBSV <sonicbsv@gmail.com>
2019-07-01 21:18:39 +0300

    Derp CAF

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_X00T.mk
---
+++ derp_X00T.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Derp stuff.
+$(call inherit-product, vendor/derp/config/derp.mk)
+
+# Inherit some common Derp stuff.
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 2160
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := derp_X00T
+PRODUCT_MODEL := ZenFone Max Pro M1
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 9 PQ3A.190505.001 5373320 release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/walleye/walleye:9/PQ3A.190505.001/5373320:user/release-keys
