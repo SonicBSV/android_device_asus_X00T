788d16c97b297fc597b76f2256ea18a3b293ec88
SonicBSV <sonicbsv@gmail.com>
2019-10-05 19:35:51 +0300

    rootdir:update fix_baseband

--- rootdir/bin/fix_baseband.sh
+++ rootdir/bin/fix_baseband.sh
@@ -1,4 +1,6 @@
-#!/system/bin/sh
+#! /vendor/bin/sh
 
 # Set GSM baseband
-setprop gsm.version.baseband `strings /vendor/firmware_mnt/image/modem.b17 | grep "^MPSS.AT" | head -1`
+if strings /vendor/firmware_mnt/image/modem.b17 | grep "^MPSS.AT" >> /dev/null; then
+    setprop gsm.version.baseband `strings /vendor/firmware_mnt/image/modem.b17 | grep "^MPSS.AT" | head -1`
+fi
