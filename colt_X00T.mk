57e520e8358d3e0ce80f53e6e5c3c04be0013847
SonicBSV <sonicbsv@gmail.com>
2019-10-16 22:42:53 +0300

    Colt OS

---
+++ colt_X00T.mk
@@ -0,0 +1,41 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Colt stuff.
+  $(call inherit-product, vendor/colt/common.mk)
+
+# Boot animation
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080x2160
+
+#For Colt's Prebuilt apk please add the following
+  $(call inherit-product, packages/apps/ColtApps/common.mk)
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := colt_X00T
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys"
+    
+# colt Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.colt.maintainer=SonicBSV
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
+
+
+
