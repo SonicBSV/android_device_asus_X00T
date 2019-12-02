5323f2403a78474a1e590a1c36b64a617cc25336
SonicBSV <sonicbsv@gmail.com>
2019-12-02 04:22:18 +0300

    Pixel Experience Q

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_X00T-userdebug
---
+++ aosp_X00T.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, build/make/target/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common PE stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := aosp_X00T
+PRODUCT_MODEL := ZenFone Max Pro M1
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.1.0.MBEMIXM release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.1.0.MBEMIXM:user/release-keys
+
+
+
