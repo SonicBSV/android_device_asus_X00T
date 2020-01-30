79de850e5b41969f57b8079b759f9c25c0ffaf74
SonicBSV <sonicbsv@gmail.com>
2020-01-30 05:30:47 +0300

    add custom fingerprint service

--- device.mk
+++ device.mk
@@ -240,6 +240,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.automotive.vehicle@1.0
 
 # Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.asus_X00T
+    
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
