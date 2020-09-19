5e1cb51f93f3e056a801dbd8d05fa2be03483a37
SonicBSV <sonicbsv@gmail.com>
2020-09-19 19:11:53 +0300

    skip abi check unused,add fingerprint from 087 Android 9 stock

--- syberia_X00T.mk
+++ syberia_X00T.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Syberia stuff.
-export SKIP_ABI_CHECKS=true
+#export SKIP_ABI_CHECKS=true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 2160
 $(call inherit-product, vendor/syberia/common.mk)
@@ -28,11 +28,11 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 22 release-keys" \
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 43 release-keys" \
     DEVICE_MAINTAINERS="SonicBSV"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.2007.084-20200706:user/release-keys
+BUILD_FINGERPRINT := asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.2009.087-20200826:user/release-keys
 
 
 
