82a6d3ad6cb9afdd755481c22ba3677bc2f496d4
SonicBSV <sonicbsv@gmail.com>
2020-10-02 05:59:41 +0300

    Resurrection Remix OS Q

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/rr_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    rr_X00T-userdebug
---
+++ ZenParts/src/com/asus/parts/BootReceiver.java
@@ -0,0 +1,38 @@
+package com.asus.parts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import android.provider.Settings;
+
+import com.asus.parts.ambient.SensorsDozeService;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    public static final  String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
+    public static final  String MICROPHONE_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
+
+    public void onReceive(Context context, Intent intent) {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        int gain = Settings.Secure.getInt(context.getContentResolver(),
+                DeviceSettings.PREF_HEADPHONE_GAIN, 5);
+        FileUtils.setValue(HEADPHONE_GAIN_PATH, gain + " " + gain);
+        FileUtils.setValue(MICROPHONE_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(),
+                DeviceSettings.PREF_MICROPHONE_GAIN, 0));
+
+        FileUtils.setValue(DeviceSettings.VIBRATION_STRENGTH_PATH, Settings.Secure.getInt(
+                context.getContentResolver(), DeviceSettings.PREF_VIBRATION_STRENGTH, 80) / 100.0 * (DeviceSettings.MAX_VIBRATION - DeviceSettings.MIN_VIBRATION) + DeviceSettings.MIN_VIBRATION);
+
+	//Ambient
+        context.startService(new Intent(context, SensorsDozeService.class));
+
+        boolean enabled = sharedPrefs.getBoolean(DeviceSettings.PREF_KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+        }
+    }
+}
