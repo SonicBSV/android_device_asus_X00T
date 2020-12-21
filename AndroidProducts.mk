d56bcd74d445d22bb602ef407ef4285d02029979
SonicBSV <sonicbsv@gmail.com>
2020-12-21 19:49:39 +0300

    Citrus-CAF

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/citrus_X00T.mk
---
+++ citrus_X00T.mk
@@ -0,0 +1,39 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Citrus stuff.
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/citrus/common.mk)
+
+# Inherit from X00T device
+$(call inherit-product,  device/asus/X00T/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := citrus_X00T
+PRODUCT_MODEL := ZenFone Max Pro M1
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=X00T PRODUCT_NAME=X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X00TD \
+    PRODUCT_NAME=WW_X00TD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 43 release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+
+
